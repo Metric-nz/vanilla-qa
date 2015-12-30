508205fd8a5c45c43c55a20d148c2bd17002ea14
Jerry Wardlow <jerrylikesbikes@gmail.com>
2015-12-29 23:26:52 -0800

    Commenting

--- qa_config.sh
+++ qa_config.sh
@@ -1,9 +1,12 @@
 apt-get -qqy update
 
+# Install Apache
 apt-get -qqy install apache2
 
+# Use Debconf for unattended MySQL installation
 debconf-set-selections <<< 'mysql-server mysql-server/root_password password SuperSecretPasswordString'
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password SuperSecretPasswordString'
+# Install MySQL Server
 apt-get -qqy install mysql-server
-
+# Install module for MySQL connections directly from PHP scripts
 apt-get -qqy install php5-mysql
