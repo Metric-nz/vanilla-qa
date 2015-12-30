8e49c9aae80954fb1b226169e50adb8e06f78e10
Jerry Wardlow <jerrylikesbikes@gmail.com>
2015-12-29 23:19:09 -0800

    Using Debconf for unattended MySQL installation

--- qa_config.sh
+++ qa_config.sh
@@ -2,6 +2,8 @@ apt-get -qqy update
 
 apt-get -qqy install apache2
 
-# !!! apt-get -qqy install mysql-server
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password SuperSecretPasswordString'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password SuperSecretPasswordString'
+apt-get -qqy install mysql-server
 
 apt-get -qqy install php5-mysql
