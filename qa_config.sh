94b7c624b8a96faaa719e522a4361851aadf6663
Jerry Wardlow <jerrylikesbikes@gmail.com>
2016-01-01 20:07:45 -0800

    Updated with variables to enhance readability

--- qa_config.sh
+++ qa_config.sh
@@ -1,11 +1,16 @@
+#!/bin/bash
+
+MYSQL_ROOT_PW='secretpassword'
+PHPMYADMIN_PW='secretphppw'
+
 apt-get -qqy update
 
 # Install Apache
 apt-get -qqy install apache2
 
 # Use Debconf for unattended MySQL installation
-debconf-set-selections <<< 'mysql-server mysql-server/root_password password SuperSecretPasswordString'
-debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password SuperSecretPasswordString'
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQL_ROOT_PW"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PW"
 # Install MySQL Server
 apt-get -qqy install mysql-server
 # Install module for MySQL connections directly from PHP scripts
@@ -20,11 +25,11 @@ apt-get -qqy install php5-mcrypt
 php5enmod mcrypt
 
 # Use Debconf for unattended phpMyAdmin installation
-debconf-set-selections <<< 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2'
-debconf-set-selections <<< 'phpmyadmin phpmyadmin/dbconfig-install boolean true'
-debconf-set-selections <<< 'phpmyadmin phpmyadmin/mysql/admin-pass password SuperSecretPasswordString'
-debconf-set-selections <<< 'phpmyadmin phpmyadmin/mysql/app-pass password vanilla'
-debconf-set-selections <<< 'phpmyadmin phpmyadmin/app-password-confirm password vanilla'
+debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $MYSQL_ROOT_PW"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PHPMYADMIN_PW"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PHPMYADMIN_PW"
 # Install phpMyAdmin and Apache utilities
 apt-get -qqy install phpmyadmin apache2-utils
 # Add phpMyAdmin to Apache configuration
