b776bdd0ff29f9e60e53f6c78532676fc829443a
Jerry Wardlow <jerrylikesbikes@gmail.com>
2016-01-05 21:23:27 -0800

    Commenting for clarity

--- qa_config.sh
+++ qa_config.sh
@@ -1,14 +1,17 @@
+#--- Global Variables ---#
 SERVERNAME='VanillaQA'
 MYSQL_ROOT_PW='secretpassword'
 PHPMYADMIN_PW='secretphppw'
 
 apt-get -qqy update
 
+#--- Apache ---#
 # Install Apache
 apt-get -qqy install apache2
 # Set ServerName
 echo -e "\nServerName \"${SERVERNAME}\"" >> /etc/apache2/apache2.conf
 
+#--- MySQL ---#
 # Use Debconf for unattended MySQL installation
 debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQL_ROOT_PW"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PW"
@@ -17,6 +20,7 @@ apt-get -qqy install mysql-server
 # Install module for MySQL connections directly from PHP scripts
 apt-get -qqy install php5-mysql
 
+#--- PHP ---#
 # Install PHP5
 apt-get -qqy install php5
 # Install PHP5 module for Apache
@@ -25,6 +29,7 @@ apt-get -qqy install libapache2-mod-php5
 apt-get -qqy install php5-mcrypt
 php5enmod mcrypt
 
+#--- phpMyAdmin ---#
 # Use Debconf for unattended phpMyAdmin installation
 debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
 debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
