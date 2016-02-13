aeafaef3618af4923b25a3989cbfcdcb3de89d17
Jerry Wardlow <jerrylikesbikes@gmail.com>
2016-02-12 23:42:19 -0800

    Added deletion of original package archive

--- go.sh
+++ go.sh
@@ -2,6 +2,8 @@
 curl -sO https://storage.googleapis.com/golang/go1.5.3.linux-amd64.tar.gz
 # Unpack package
 tar -xf go1.5.3.linux-amd64.tar.gz
+# Delete package archive
+rm go1.5.3.linux-amd64.tar.gz
 # Move installation
 mv go /usr/local
 # Set Go path
