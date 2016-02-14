63f673c987b57509a3aa39da9cd57b43957c51d6
Jerry Wardlow <jerrylikesbikes@gmail.com>
2016-02-13 20:52:32 -0800

    Replaced Docker installation with direct

---
+++ prom_prov.sh
@@ -0,0 +1,10 @@
+# Get package archive for Prometheus
+wget -q "https://github.com/prometheus/prometheus/releases/download/0.15.1/prometheus-0.15.1.linux-amd64.tar.gz" -P /prometheus
+# Create installation directory
+mkdir -p /usr/local/prometheus/server
+# Extract archive
+tar -xf /prometheus/prometheus-0.15.1.linux-amd64.tar.gz -C /usr/local/prometheus/server
+# Copy configuration file into installation directory
+cp /prometheus/prometheus.yml /usr/local/prometheus/server/
+# Set path
+echo 'export PATH=$PATH:/usr/local/prometheus/server' >> /home/vagrant/.profile
