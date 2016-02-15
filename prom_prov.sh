d5e7410288d523b3460a9f7748c094aaea68a841
Jerry Wardlow <jerrylikesbikes@gmail.com>
2016-02-14 16:24:54 -0800

    Removed copying of configuration file

--- prom_prov.sh
+++ prom_prov.sh
@@ -5,7 +5,5 @@ wget -q "https://github.com/prometheus/prometheus/releases/download/0.15.1/prome
 mkdir -p /usr/local/prometheus/server
 # Extract archive
 tar -xf /prometheus/prometheus-0.15.1.linux-amd64.tar.gz -C /usr/local/prometheus/server
-# Copy configuration file into installation directory
-cp /prometheus/prometheus.yml /usr/local/prometheus/server/
 # Set path
 echo 'export PATH=$PATH:/usr/local/prometheus/server' >> /home/vagrant/.profile
