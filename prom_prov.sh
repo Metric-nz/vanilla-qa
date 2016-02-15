86324de483727676e18e25c1d29df88bb8b6b21c
Jerry Wardlow <jerrylikesbikes@gmail.com>
2016-02-14 16:53:50 -0800

    Added basic installation of Node Exporter

--- prom_prov.sh
+++ prom_prov.sh
@@ -8,3 +8,12 @@ tar -xf /prometheus/prometheus-0.15.1.linux-amd64.tar.gz -C /usr/local/prometheu
 rm /prometheus/prometheus-0.15.1.linux-amd64.tar.gz
 # Set path
 echo 'export PATH=$PATH:/usr/local/prometheus/server' >> /home/vagrant/.profile
+
+#--- Node Exporter ---#
+# Pull down Node Exporter archive
+wget -q "https://github.com/prometheus/node_exporter/releases/download/0.11.0/node_exporter-0.11.0.linux-amd64.tar.gz" -P /prometheus
+# Create installation directory
+mkdir /usr/local/prometheus/node_exporter
+# Extract archive and remove
+tar -xf /prometheus/node_exporter-0.11.0.linux-amd64.tar.gz -C /usr/local/prometheus/node_exporter
+rm /prometheus/node_exporter-0.11.0.linux-amd64.tar.gz
